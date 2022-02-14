c4a8022016e4912501012dfb3e2b3c5f33f0b972
Adamocho <81510507+Adamocho@users.noreply.github.com>
2022-02-14 12:36:57 +0100

    Change variable names and cleanup

--- Doomer.py
+++ Doomer.py
@@ -17,7 +17,7 @@ class App(QWidget):
         self.win_width = 450
         self.win_height = 450
 
-        self.gamel = {
+        self.titles = {
             'Ultimate Doom': 'doom.wad',
             'Doom II': 'doom2.wad',
             'Doom II: TNT Evilution': 'tnt.wad',
@@ -25,7 +25,7 @@ class App(QWidget):
         }
         #'Doom II: Master Levels': 'doom2.wad -file'
 
-        self.masterl = {
+        self.wads = {
             '': 'wads/attack.wad',
             1: 'wads/attack.wad',
             2: 'wads/canyon.wad',
@@ -49,7 +49,7 @@ class App(QWidget):
             20: 'wads/teeth.wad'
         }
 
-        self.diffl = {
+        self.diff_lvls = {
             'ITYTD': 1,
             'HNTR': 2,
             'HMP': 3,
@@ -69,91 +69,96 @@ class App(QWidget):
         #   Now there's some UI
 
         # Label = canvas for image
-        self.l1 = QtWidgets.QLabel(self)                                                                                
+        self.label = QtWidgets.QLabel(self)                                                                                
         # Assign img to label + scale it to match window size
-        self.l1.setPixmap(QPixmap('icons/prbico1.png').scaled(self.win_width, self.win_height))
+        self.label.setPixmap(QPixmap('icons/prbico1.png').scaled(self.win_width, self.win_height))
 
         # wad, skill, warp, nomonsters, fast, record, fastdemo, timedemo, playdemo, nomusic, nomouse, net
 
-        # Game
-        self.combo1 = QtWidgets.QComboBox(self)                                                                        
-        self.combo1.setGeometry(5, 5, 300, 30)
-        self.combo1.addItems(self.gamel)
+        # Game title combo box
+        self.title_combo = QtWidgets.QComboBox(self)                                                                        
+        self.title_combo.setGeometry(5, 5, 300, 30)
+        self.title_combo.addItems(self.titles)
 
-        # Difficulty
-        self.combo2 = QtWidgets.QComboBox(self)                                                                        
-        self.combo2.setGeometry(315, 5, 125, 30)
-        self.combo2.addItems(self.diffl)
-        # Default is HMP
-        self.combo2.setCurrentIndex(2)                                                                                  
+        # Difficulty combo box
+        self.diff_combo = QtWidgets.QComboBox(self)                                                                        
+        self.diff_combo.setGeometry(315, 5, 125, 30)
+        self.diff_combo.addItems(self.diff_lvls)
+        # HMP is set by default
+        self.diff_combo.setCurrentIndex(2)                                                                                  
 
-        self.cbox1 = QtWidgets.QCheckBox('Record', self)
-        self.cbox1.setGeometry(170, 40, 100, 30)
+        # Record checkbox
+        self.record_cbx = QtWidgets.QCheckBox('Record', self)
+        self.record_cbx.setGeometry(170, 40, 100, 30)
 
-        self.cbox2 = QtWidgets.QCheckBox('Fast', self)
-        self.cbox2.setGeometry(40, 80, 100, 30)
+        # Fast checkbox
+        self.fast_cbx = QtWidgets.QCheckBox('Fast', self)
+        self.fast_cbx.setGeometry(40, 80, 100, 30)
 
-        self.cbox3 = QtWidgets.QCheckBox('No Monsters', self)
-        self.cbox3.setGeometry(310, 50, 130, 30)
+        self.nomo_cbx = QtWidgets.QCheckBox('No Monsters', self)
+        self.nomo_cbx.setGeometry(310, 50, 130, 30)
 
-        self.cbox4 = QtWidgets.QCheckBox('No Music', self)
-        self.cbox4.setGeometry(335, 110, 110, 30)
+        self.music_cbx = QtWidgets.QCheckBox('Music', self)
+        self.music_cbx.setGeometry(335, 110, 110, 30)
 
-        self.cbox5 = QtWidgets.QCheckBox('No Mouse', self)
-        self.cbox5.setGeometry(330, 80, 110, 30)
+        self.mouse_cbx = QtWidgets.QCheckBox('Mouse', self)
+        self.mouse_cbx.setGeometry(330, 80, 110, 30)
 
-        self.cbox6 = QtWidgets.QCheckBox('Respawn', self)
-        self.cbox6.setGeometry(25, 50, 100, 30)
+        self.respawn_cbx = QtWidgets.QCheckBox('Respawn', self)
+        self.respawn_cbx.setGeometry(25, 50, 100, 30)
 
-        self.button1 = QtWidgets.QPushButton('Play\Join Game', self)
-        self.button1.setGeometry(5, 395, 200, 50)
-        self.button1.clicked.connect(self.on_click)
+        self.launch_btn = QtWidgets.QPushButton('Play\Join Game', self)
+        self.launch_btn.setGeometry(5, 395, 200, 50)
+        self.launch_btn.clicked.connect(self.on_click)
 
-        #self.button2 = QtWidgets.QPushButton('Start Server', self)
-        #self.button2.setGeometry(295, 395, 150, 50)
-        #self.button2.clicked.connect(self.on_click_server)   
+        #self.server_btn = QtWidgets.QPushButton('Start Server', self)
+        #self.server_btn.setGeometry(295, 395, 150, 50)
+        #self.server_btn.clicked.connect(self.on_click_server)   
             # UNKOMMENT THIS WHEN YOU FINISH WORKING ON SERVER SCRIPT!!!!!!!!!!!!!
 
-        self.le1 = QtWidgets.QLineEdit(self)
-        self.le1.setGeometry(30, 200, 100, 35)
-        self.le1.setPlaceholderText('Map')
+        self.map_le = QtWidgets.QLineEdit(self)
+        self.map_le.setGeometry(30, 200, 100, 35)
+        self.map_le.setPlaceholderText('Map')
 
-        self.le2 = QtWidgets.QLineEdit(self)
-        self.le2.setGeometry(150, 200, 250, 35)
-        self.le2.setPlaceholderText('IP for multiplayer game')
+        self.ip_le = QtWidgets.QLineEdit(self)
+        self.ip_le.setGeometry(150, 200, 250, 35)
+        self.ip_le.setPlaceholderText('IP for multiplayer game')
 
-        self.show()                                                                                                     
+        self.show()
 
     # Logic for running the game with chosen settings
-    def on_click(self):              
-        if self.combo1.currentText() == 'Doom II: Master Levels':
-            batstring = f'prboom-plus -iwad wads/{self.gamel[self.combo1.currentText()]} {self.masterl[self.le1.text()]} -skill {str(self.diffl[self.combo2.currentText()])}'
-        else:
-            batstring = f'prboom-plus -iwad wads/{self.gamel[self.combo1.currentText()]} -skill {str(self.diffl[self.combo2.currentText()])}'
+    def on_click(self):  
+            # For master levels implementation
+        # if self.title_combo.currentText() == 'Doom II: Master Levels':
+        #     command = f'prboom-plus \
+        #         -iwad wads/{self.titles[self.title_combo.currentText()]} {self.wads[self.map_le.text()]} \
+        #         -skill {str(self.diff_lvls[self.diff_combo.currentText()])}'
+        
+        command = f'prboom-plus -iwad wads/{self.titles[self.title_combo.currentText()]} -skill {str(self.diff_lvls[self.diff_combo.currentText()])}'
 
-        if self.le1.text() != '' and self.combo1.currentText() != 'Doom II: Master Levels':
-            batstring += f' -warp {self.le1.text()}'
-        if self.le2.text() != '':
-            batstring += f' -net {self.le2.text()}'
-        if self.cbox1.isChecked():
-            batstring += f' -record {datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}'
-        if self.cbox2.isChecked():
-            batstring += ' -fast'
-        if self.cbox3.isChecked():
-            batstring += ' -nomonsters'
-        if self.cbox4.isChecked():
-            batstring += ' -nomusic'
-        if self.cbox5.isChecked():
-            batstring += ' -nomouse'
-        if self.cbox6.isChecked():
-            batstring += ' -respawn'
+        if self.map_le.text() != '' and self.title_combo.currentText() != 'Doom II: Master Levels':
+            command += f' -warp {self.map_le.text()}'
+        if self.ip_le.text() != '':
+            command += f' -net {self.ip_le.text()}'
+        if self.record_cbx.isChecked():
+            command += f' -record {datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}'
+        if self.fast_cbx.isChecked():
+            command += ' -fast'
+        if self.nomo_cbx.isChecked():
+            command += ' -nomonsters'
+        if self.music_cbx.isChecked():
+            command += ' -nomusic'
+        if self.mouse_cbx.isChecked():
+            command += ' -nomouse'
+        if self.respawn_cbx.isChecked():
+            command += ' -respawn'
 
-        os.system(batstring)
+        os.system(command)
 
 
-    # For sever logic
-    # def on_click_server(self):
-    #    print(5)
+    # Server logic
+    def on_click_server(self):
+        print("Server")
 
 # If this is the main file
 if __name__ == '__main__':
